c4239d4592b52c4f84d12b5e1a3830d1e13c923f
pretasouzamirandadesouza-maker <Pretasouzamirandadesouza@gmail.com>
2026-04-09 23:59:17 -0300

    fix bootsplash hide no app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
+import RNBootSplash from "react-native-bootsplash";
 import React, { useEffect } from 'react';
 import { BackHandler } from 'react-native';
 import { NavigationRouter } from './src/routers/navigation-router';
 
 export const App = () => {
   useEffect(() => {
+    RNBootSplash.hide({ fade: true });
     const backAction = () => {
       return true;
     };
